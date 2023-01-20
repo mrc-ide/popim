4f050a57a006a0b9acb9ad2e3c64643784ab4b5d
Tini Garske <t.garske@imperial.ac.uk>
2023-01-20 14:18:16 +0000

    debugging new test for apply_vacc

--- tests/testthat/test-apply_vacc.R
+++ tests/testthat/test-apply_vacc.R
@@ -7,7 +7,7 @@ test_that("apply_vacc modifies the correct rows", {
     ## first expected value:
     ee1 <- data.frame(year = 2001:2005, age = 1:5, cohort = 2000, immunity = 0.2)
 
-    expect_true(all(pop[pop$immunity == 0.2,] == ee1))
+    expect_true(all(df[df$immunity == 0.2,] == ee1))
 
     ## adding a previous campaign on top:
     df <- apply_vacc(df, year = 1995, age_first = 0, age_last = 5,
@@ -15,8 +15,8 @@ test_that("apply_vacc modifies the correct rows", {
 
     ee2 <- list(year = 2000, age = 5, cohort = 1995, immunity = 0.5)
 
-    expect_true(all(pop[pop$immunity == 0.5,] == ee2))
-    expect_true(all(pop[pop$immunity == 0.2,] == ee1)) ## no change to
+    expect_true(all(df[df$immunity == 0.5,] == ee2))
+    expect_true(all(df[df$immunity == 0.2,] == ee1)) ## no change to
                                                        ## the other
                                                        ## vaccinated
                                                        ## cohorts
