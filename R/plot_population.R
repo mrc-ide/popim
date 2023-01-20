e96114241299ed53498b5189fa69fa1e15cbac4d
Tini Garske <t.garske@imperial.ac.uk>
2023-01-20 13:01:02 +0000

    debugging note on function plot_population

--- R/plot_population.R
+++ R/plot_population.R
@@ -14,7 +14,7 @@ plot_population <- function(pop_df) {
     ## option to pass a colour scheme
 
     g <- ggplot2::ggplot(pop_df) +
-        ggplot2::aes(x = year, y = age, fill = immunity) +
+        ggplot2::aes(x = pop_df$year, y = pop_df$age, fill = pop_df$immunity) +
         ggplot2::geom_tile() +
         ggplot2::scale_fill_gradient(low = "white",
                                      high = pal[5],
