e8623dda17b313af0684c5ca879a13c7c3b3bc1e
Tini Garske <t.garske@imperial.ac.uk>
2023-01-20 14:12:24 +0000

    new test for apply_vacc

---
+++ tests/testthat/test-apply_vacc.R
@@ -0,0 +1,24 @@
+test_that("apply_vacc modifies the correct rows", {
+
+    df <- setup_population(2000, 2005, 0, 5)
+    df <- apply_vacc(df, year = 2000, age_first = 0, age_last = 0,
+                     coverage = 0.2, skew = 0)
+
+    ## first expected value:
+    ee1 <- data.frame(year = 2001:2005, age = 1:5, cohort = 2000, immunity = 0.2)
+
+    expect_true(all(pop[pop$immunity == 0.2,] == ee1))
+
+    ## adding a previous campaign on top:
+    df <- apply_vacc(df, year = 1995, age_first = 0, age_last = 5,
+                     coverage = 0.5, skew = 0)
+
+    ee2 <- list(year = 2000, age = 5, cohort = 1995, immunity = 0.5)
+
+    expect_true(all(pop[pop$immunity == 0.5,] == ee2))
+    expect_true(all(pop[pop$immunity == 0.2,] == ee1)) ## no change to
+                                                       ## the other
+                                                       ## vaccinated
+                                                       ## cohorts
+
+})
