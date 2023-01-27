11e12cf8be6855b6f214ac96bcce91226ec18c75
Tini Garske <t.garske@imperial.ac.uk>
2023-01-27 11:12:59 +0000

    improving style in tests

--- tests/testthat/test-apply_vacc.R
+++ tests/testthat/test-apply_vacc.R
@@ -8,7 +8,7 @@ test_that("apply_vacc modifies the correct rows", {
     ee1 <- data.frame(year = 2001:2005, age = 1:5, cohort = 2000,
                       immunity = 0.2)
 
-    expect_true(all(df[df$immunity > 0,] == ee1))
+    expect_equal(df[df$immunity > 0,], ee1, ignore_attr = TRUE)
 
     ## adding a previous campaign on top:
     df <- apply_vacc(df, year = 1996, age_first = 0, age_last = 5,
@@ -17,8 +17,8 @@ test_that("apply_vacc modifies the correct rows", {
     ee2 <- data.frame(year = c(2000, 2000, 2001), age = c(4, 5, 5),
                       cohort = c(1996, 1995, 1996), immunity = 0.5)
 
-    expect_true(all(df[df$immunity == 0.5,] == ee2))
+    expect_equal(df[df$immunity == 0.5,], ee2, ignore_attr = TRUE)
     ## no change to the other vaccinated cohorts:
-    expect_true(all(df[df$immunity == 0.2,] == ee1))
+    expect_equal(df[df$immunity == 0.2,], ee1, ignore_attr = TRUE)
 
 })
--- tests/testthat/test-is_population.R
+++ tests/testthat/test-is_population.R
@@ -4,38 +4,39 @@ test_that("setup_population generates a df that passes the is_population check",
 })
 
 test_that("is_population rejects df with missing columns", {
-    expect_false({
-        df <- setup_population(2000, 2010)
-        df$cohort <- NULL
-        is_population(df)})
+
+    df <- setup_population(2000, 2010)
+    df$cohort <- NULL
+
+    expect_false(is_population(df))
 })
 
 test_that("is_population rejects df with incorrectly typed columns", {
-    expect_false({
-        df <- setup_population(2000, 2010, 0, 10)
-        df$year <- "a"
-        is_population(df)})
+
+    df <- setup_population(2000, 2010, 0, 10)
+    df$year <- "a"
+
+    expect_false(is_population(df))
 })
 
 test_that("is_population rejects df with negative ages", {
-    expect_false({
-        df <- setup_population(2000, 2005, 0, 5)
-        df$age <- df$age - 2
-        is_population(df)
-    })
+
+    df <- setup_population(2000, 2005, 0, 5)
+    df$age <- df$age - 2
+
+    expect_false(is_population(df))
 })
 
 test_that("is_population rejects df with immunity outside (0, 1)", {
-    expect_false({
-        df <- setup_population(2000, 2005, 0, 5)
-        df$immunity[5] <- -1
-        is_population(df)
-    })
-    expect_false({
-        df <- setup_population(2000, 2005, 0, 5)
-        df$immunity[5] <- 1.3
-        is_population(df)
-    })
+
+    df <- setup_population(2000, 2005, 0, 5)
+    df$immunity[5] <- -1
+
+    expect_false(is_population(df))
+
+    df <- setup_population(2000, 2005, 0, 5)
+    df$immunity[5] <- 1.3
+    expect_false(is_population(df))
 })
         
         
