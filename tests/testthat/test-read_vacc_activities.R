f10ee76318bb94aecb59ab385ad4c660289c9c67
Tini Garske <t.garske@imperial.ac.uk>
2023-04-20 12:58:32 +0100

    tests for read_vacc_activities

---
+++ tests/testthat/test-read_vacc_activities.R
@@ -0,0 +1,19 @@
+test_that("read_vacc_activities reads well", {
+    infile <- system.file("extdata/vacc_activities.csv", package = "vip")
+
+    ## reading a good file correctly
+    vaccs <- read_vacc_activities(infile)
+    expect_null(validate_vacc_activities(vaccs))
+
+    ## error for reading a bad file
+    vaccs$target <- NULL
+    write.csv(vaccs, "tmp.csv")
+
+    expect_error(vaccs <- read_vacc_activities("tmp.csv"))
+    file.remove("tmp.csv")
+
+    ## error for reading non-existing file
+    infile_bad <- paste0(infile, "bad")
+    expect_error(vaccs <- read_vacc_acitivies(infile_bad))
+
+})
