43f1a3de799c0b76a9226194500c654ac35136f3
Tini Garske <t.garske@imperial.ac.uk>
2023-11-24 12:31:09 +0000

    add test for read_population and sample population data csv file

---
+++ tests/testthat/test-read_population.R
@@ -0,0 +1,20 @@
+test_that("read_population reads well", {
+    infile <- system.file("extdata/pop_sample.csv", package = "vip")
+
+    ## reading a good file correctly
+    pop <- read_population(infile)
+    expect_true(is_population(pop))
+
+    ## error for reading a bad file
+    pop$pop_size <- NULL
+    write.csv(pop, "tmp.csv")
+
+    expect_error(pop <- read_population("tmp.csv"))
+    file.remove("tmp.csv")
+
+    ## error for reading non-existing file
+    infile_bad <- paste0(infile, "bad")
+    expect_error(pop <- read_population(infile_bad))
+
+
+})
