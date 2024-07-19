6ae3645c8c3524be799c51d674b5b8b744df65b8
Tini Garske <t.garske@imperial.ac.uk>
2024-07-19 13:37:05 +0100

    comment out non-working wip code

--- R/unwpp.R
+++ R/unwpp.R
@@ -3,14 +3,14 @@
 base_url <- "http://population.un.org/dataportalapi/api/v1"
 target <- paste0(base_url,
                  "/data/indicators/47/locations/566/start/1995/end/2000")
-js <- jsonlite::fromJSON(target)
+## js <- jsonlite::fromJSON(target)
 ## indicator 47: population by 1 year age groups and by sex"
 ## location 566: Nigeria
 
 ## copy json data from website and save as file "inst/extdata/2000.json".
 
-js <- jsonlite::fromJSON("inst/extdata/2000.json")
-js_2 <- jsonlite::fromJSON("inst/extdata/2000_2.json")
+## js <- jsonlite::fromJSON("inst/extdata/2000.json")
+## js_2 <- jsonlite::fromJSON("inst/extdata/2000_2.json")
 ## js_3 <- jsonlite::fromJSON("inst/extdata/2000_3.json")
 ## js_4 <- jsonlite::fromJSON("inst/extdata/2000_4.json")
 
@@ -29,21 +29,21 @@ js_2 <- jsonlite::fromJSON("inst/extdata/2000_2.json")
 ## df <- as_popim_pop(df)
 
 
-library(jsonlite)
-library(httr)
+## library(jsonlite)
+## library(httr)
 
-base_url <- "https://population.un.org/dataportalapi/api/v1"
-target <- paste0(base_url, "/indicators/")
-target <- paste0(base_url, "/locations/")
+## base_url <- "https://population.un.org/dataportalapi/api/v1"
+## target <- paste0(base_url, "/indicators/")
+## target <- paste0(base_url, "/locations/")
 
-response <- fromJSON(target)
+## response <- fromJSON(target)
 
-data1 <- fromJSON("https://api.github.com/users/hadley/orgs")
-## fromJSON works with an appropriate url.
+## data1 <- fromJSON("https://api.github.com/users/hadley/orgs")
+## ## fromJSON works with an appropriate url.
 
-## URL for a server ssl protocol scan:
-## https://www.ssllabs.com/ssltest/
-## https://www.ssllabs.com/ssltest/analyze.html?d=population.un.org
+## ## URL for a server ssl protocol scan:
+## ## https://www.ssllabs.com/ssltest/
+## ## https://www.ssllabs.com/ssltest/analyze.html?d=population.un.org
 
-## maybe this is to do with the openssl version I've got?
-## in the shell: openssl version
+## ## maybe this is to do with the openssl version I've got?
+## ## in the shell: openssl version
